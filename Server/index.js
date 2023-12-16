27c66866f7b2fa63f1ef36b07bc9987960535780
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-12-16 22:00:17 +0600

    vercel file change

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ const {
     app,
     mongoose,
     DATA
-} = require('./App');
+} = require('./app');
 
 // Database connection.... 
 const dataConnect = async () => {
@@ -16,9 +16,9 @@ const dataConnect = async () => {
 };
 
 // running server.....
-app.listen(PORT||8500, async () => {
+app.listen(PORT, async () => {
     try {
-        console.log(`Sever run successfully on http://localhost:${PORT||8500}`);
+        console.log(`Sever run successfully on http://localhost:${PORT}`);
         await dataConnect();
     } catch (error) {
         console.log(error.message);
